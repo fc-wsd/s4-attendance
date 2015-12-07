3361c9bfccec5457077e12b5406e44f2515b415c
sagan9 <guanming.kim@gmail.com>
2015-12-07 21:52:54 +0900

    한글 테스트 (이제야) 완료 ㅠ.ㅠ

--- sagan9_attendance1.py
+++ sagan9_attendance1.py
@@ -5,8 +5,8 @@ import time
 
 # 학생 명단이 있는 객체 `students` 명단
 # Sublime Text 3 에서 build시 UnicodeEncodeError 가 발생하여, 일단은 영어로 작성
-# students = ["김용열", "김상범", "박순우", "이준범", "정회웅", "황준식"]
-students = ["Yongyeol", "Sangbeom", "Soonwoo", "Junbeom", "Hoiwoong", "Junsik"]
+students = ["김용열", "김상범", "박순우", "이준범", "정회웅", "황준식"]
+# students = ["Yongyeol", "Sangbeom", "Soonwoo", "Junbeom", "Hoiwoong", "Junsik"]
 
 # 출석자는 `attendances` 객체에 저장하며, 출석 일시와 이름은 필수 저장 항목.
 attendances = {}
